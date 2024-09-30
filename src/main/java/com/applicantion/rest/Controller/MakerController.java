cec53eb60610ae44ebf9adcc5a44b25c78661442
ioskpu <lrcorales@gmail.com>
2024-09-30 08:20:32 -0500

    se completa el controlador para la entidad Maker(fabricante)

--- src/main/java/com/applicantion/rest/Controller/MakerController.java
+++ src/main/java/com/applicantion/rest/Controller/MakerController.java
@@ -71,16 +71,81 @@ public ResponseEntity<?> findAll(){
     return ResponseEntity.ok(makerServices.findAll());
 }
 
+/**
+ * Guarda la información del fabricante
+ *
+ * @param makerDTO Objeto DTO que contiene la información del fabricante
+ * @return ResponseEntity<?> Entidad de respuesta
+ *
+ * Este método recibe una solicitud POST y guarda la información del fabricante.
+ * Si el nombre del fabricante está vacío, se devuelve una respuesta de solicitud incorrecta;
+ * de lo contrario, se guarda la información del fabricante en la base de datos y se devuelve
+ * una entidad de respuesta creada. El método utiliza el anotación @RequestBody para
+ * vincular los datos del cuerpo de la solicitud HTTP con el parámetro makerDTO y
+ * construye un objeto Maker mediante Maker.builder() para guardar la información.
+ */
 @PostMapping("/save")
 public ResponseEntity<?> save(@RequestBody MakerDTO makerDTO) throws URISyntaxException {
 
+    // Verifica si el nombre del fabricante está vacío, en cuyo caso se devuelve una solicitud incorrecta
     if(makerDTO.getName().isBlank()){
         return ResponseEntity.badRequest().build();
     }
 
+    // Guarda la información del fabricante en la base de datos
     makerServices.save(Maker.builder()
             .name(makerDTO.getName())
             .build());
+
+    // Devuelve una entidad de respuesta creada, indicando que el recurso se ha creado correctamente
     return ResponseEntity.created(new URI("/api/maker/save")).build();
 }
+
+
+/**
+ * Método del controlador para actualizar la información de un Maker específico
+ *
+ * @param id Identificador único del Maker que se desea actualizar
+ * @param makerDTO Objeto MakerDTO que contiene la información actualizada, enviado a través del cuerpo de la solicitud
+ * @return Retorna un objeto ResponseEntity que contiene el resultado del proceso
+ */
+@PutMapping("/update/{id}")
+public ResponseEntity<?> updateMaker(@PathVariable Long id, @RequestBody MakerDTO makerDTO){
+    // Intenta encontrar el Maker con el ID especificado a través del método findById del servicio
+    Optional<Maker> makerOptional = makerServices.findById(id);
+
+    // Verifica si se encontró el Maker con el ID especificado
+    if(makerOptional.isPresent()){
+        // Si se encontró, obtiene el objeto Maker
+        Maker maker = makerOptional.get();
+        // Actualiza el nombre del Maker
+        maker.setName(makerDTO.getName());
+        // Guarda el Maker actualizado
+        makerServices.save(maker);
+        // Retorna un ResponseEntity que indica éxito y contiene un mensaje de confirmación de actualización
+        return ResponseEntity.ok("Registro Actualizado");
+    }
+
+    // Si no se encontró el Maker con el ID especificado, retorna un ResponseEntity que indica que no se encontró
+    return ResponseEntity.notFound().build();
+}
+
+/**
+ * Maneja una solicitud de eliminación para eliminar un registro de Maker por su ID
+ *
+ * @param id El ID del registro de Maker a eliminar
+ * @return Devuelve un estado HTTP 200 con el mensaje "Registro eliminado" si la eliminación es exitosa, o un estado 400 si la solicitud es incorrecta
+ */
+@DeleteMapping("/delete/{id}")
+public ResponseEntity<?> deleteMaker(@PathVariable Long id){
+    // Verifica si el ID no es nulo, en ese caso procede con la eliminación
+    if(id != null){
+        makerServices.deleteById(id);
+        return ResponseEntity.ok("Registro eliminado");
+    }
+
+    // Si el ID es nulo, devuelve una solicitud incorrecta
+    return ResponseEntity.badRequest().build();
+}
+
 }
